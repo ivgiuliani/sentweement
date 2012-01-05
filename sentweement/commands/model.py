a8d4203f744d8206117e18ca8900d15855198524
Ivan Giuliani <giuliani.v@gmail.com>
2012-01-05 19:28:16 +0100

    Added model creation command

--- sentweement/commands/model.py
+++ sentweement/commands/model.py
@@ -1,7 +1,34 @@
-from sentweement.commands.base import BaseCommand
+from sentweement import tweet
+from sentweement.datareader import DataReader
+from sentweement.learning import SentimentModel
+from sentweement.commands.base import BaseCommand, InvalidParameters
+
+import sys
 
 class CreateModelCommand(BaseCommand):
-    pass
+    def train_over_dumps(self, filenames):
+        model = SentimentModel()
+        reader = DataReader(filenames)
+
+        for tweet in reader.get_tweets():
+            model.fit(tweet)
+
+        return model
+
+    def run(self):
+        args = self.get_arguments()
+        try:
+            model_file = args[0]
+            datasets = args[1:]
+        except IndexError:
+            raise InvalidParameters
+
+        sys.stdout.write("Processing %d files... " % len(datasets))
+        sys.stdout.flush()
+        self.train_over_dumps(datasets)
+        sys.stdout.write(" done!\n")
+
+        return False
 
 class UpdateModelCommand(BaseCommand):
     pass
