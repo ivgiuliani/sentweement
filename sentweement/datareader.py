6921151169591b395f1748e94726a74c62425169
Ivan Giuliani <giuliani.v@gmail.com>
2012-01-05 23:05:18 +0100

    add file-change notify event through a callback within the datareader

--- sentweement/datareader.py
+++ sentweement/datareader.py
@@ -4,11 +4,12 @@ import os
 class DataReader(object):
     "Performs unified data reading over a set of given tweet dumps"
 
-    def __init__(self, filenames=None):
-        self._filenames = filenames or []
+    def __init__(self, filenames=None, file_changed_callback=None):
+        self.__filenames = filenames or []
+        self.__file_changed_callback = file_changed_callback
 
-        for filename in self._filenames:
-            if not self._files_exist(self._filenames):
+        for filename in self.__filenames:
+            if not self.__files_exist(self.__filenames):
                 # check that the files exist before starting to avoid abrupt
                 # interruptions in the middle of a computation
                 err = "read error: %s doesn't exist or cannot be read"
@@ -16,7 +17,10 @@ class DataReader(object):
 
     def get_tweets(self):
         "Iterates over the whole list of tweets available"
-        for filename in self._filenames:
+        for filename in self.__filenames:
+            if self.__file_changed_callback:
+                self.__file_changed_callback(filename)
+
             reader = csv.reader(open(filename, "rb"))
 
             for row in reader:
@@ -30,7 +34,7 @@ class DataReader(object):
 
         raise StopIteration
 
-    def _files_exist(self, filenames):
+    def __files_exist(self, filenames):
         "Check that the given list of filenames exist"
         for filename in filenames:
             if not os.path.exists(filename):
